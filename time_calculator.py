e3d8ef8607f45b6bd26df9139c4d27ce177b553e
Horsebeean <130872600+Horsebeean@users.noreply.github.com>
2024-05-01 17:36:30 +0800

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,27 +1,6 @@
 CONST_616 = 16 # 616 是 SPEED_UP_COST_DIVISOR
 
-# getSpeedUpCostWithMultiplier(100,604800,604800) 7天
-# 期待值:1179。python计算值1179,c#计算值1179
-
-# getSpeedUpCostWithMultiplier(100,604800,604800) 8天
-# 期待值:1304。python计算值1304,c#计算值1304
-
-# getSpeedUpCostWithMultiplier(100,864000,864000) 10天
-# 期待值:1543。python计算值1543,c#计算值1543
-
-# getSpeedUpCostWithMultiplier(100,1036800,1036800) 12天
-# 期待值:1769。python计算值1769,c#计算值1769
-
-# getSpeedUpCostWithMultiplier(100,1209600,1209600) 14天
-# 期待值:1900。python计算值1985,c#计算值-1500
-
-# getSpeedUpCostWithMultiplier(100,1814400,1814400) 21天
-# 期待值: 325。python计算值2692,c#计算值325
-
-# getSpeedUpCostWithMultiplier(100,2592000,2592000) 30天，此时游戏内已不显示进度条
-# 期待值: 1。python计算值3518,c#计算值(出错:不能对负数开平方)
-
-# 所以，不建议将升级/建造时间调至12天以上。
+# 不建议将升级/建造时间调至12天以上。
 
 def sqrt(base): return int(base**(1/2))
 
@@ -52,5 +31,7 @@ def getSpeedUpCostWithMultiplierOptimized(total_time,remaining_time):
     return max(1,getSpeedUpCostWithMultiplier(100,total_time,remaining_time))
 
 if __name__=="__main__":
+    print("== Time Calculator ==")
+    print("Press [Ctrl] + [C] to quit.")
     while(1):
-        print(getSpeedUpCostWithMultiplier(100,int(input("总时间(秒)：")),int(input("剩余时间(秒)："))))
+        print(getSpeedUpCostWithMultiplier(100,int(input("Total Time(Sec)：")),int(input("Remaining Time(Sec)："))))
